1670c0a4224a939160d218b645185404f611d0e3
Kuba Kazmierczak <jakub.kazmierczak@netguru.com>
2020-11-30 13:01:21 +0100

    Add error endpoint

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 class PagesController < ApplicationController
-  def home
-    
+  def home; end
+
+  def error
+    1 / 0
+  rescue ZeroDivisionError => e
+    Raven.capture_exception(e)
   end
 end
